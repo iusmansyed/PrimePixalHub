6a1c283ddc4f7ceedfdc7e3220d1964b200bedc4
Ameer Hamza <ameer.hamza@Blaze.com>
2023-11-21 20:24:46 +0500

    changes

--- src/Components/TawkTo/TawkToConfig.jsx
+++ src/Components/TawkTo/TawkToConfig.jsx
@@ -6,7 +6,7 @@ function ChatWidget() {
 
     scriptElement.type = 'text/javascript';
     scriptElement.async = true;
-    scriptElement.src = 'https://embed.tawk.to/654d2bfacec6a912820e59ee/1heqn7js1'; // Update the src with your desired URL
+    scriptElement.src = 'https://embed.tawk.to/655bcac0d600b968d3153d00/1hfn900uf'; // Update the src with your desired URL
     scriptElement.charset = 'UTF-8';
     scriptElement.setAttribute('crossorigin', '*');
     document.head.appendChild(scriptElement);
